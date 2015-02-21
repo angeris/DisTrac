8dab1b313774ce22ef0bce3fe7ac9d91ba1d2fc7
clay-to-n <cbrand@usc.edu>
2015-02-21 05:00:41 -0800

    World exists

--- DisTracApp/client/templates/index/index.js
+++ DisTracApp/client/templates/index/index.js
@@ -7,46 +7,73 @@ var controls;
 var start = Date.now();
 
 Template.index.rendered = function () {
-  init();
+  Meteor.setTimeout(init, 0);
 };
 
 
 function init() {
   
-  scene = new THREE.Scene();
   
-  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  camera.position.y = 150;
-  camera.position.z = 500;
-    
-  renderer = new THREE.CanvasRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  
-  var light = new THREE.PointLight( 0xffffff);
-  light.position.set(500,500,500);
-  scene.add(light);
-  
-  var geometry = new THREE.CubeGeometry(100,100,100);
-  var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-  var cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
-  
-  console.log(renderer.domElement);
-  $("body").append(renderer.domElement);
-  
-  //Blaze.render("index", document.body, renderer.domElement, "index");
-  //UI.insert(UI.render(renderer.domElement), document.body)
+  if(!Detector.webgl){
+      Detector.addGetWebGLMessage();
+    } else {
 
+      container = $(".container");
+      var years = ['1990','1995','2000'];
+      var globe = new DAT.Globe(container);
+      
+      globe.animate();
+      document.body.style.backgroundImage = 'none'; // remove loading
+
+      
+      
+      
+      
+      /*container.addEventListener('mousedown', onMouseDown, false);
+
+    container.addEventListener('mousewheel', onMouseWheel, false);
+
+    document.addEventListener('keydown', onDocumentKeyDown, false);
+    */
+
+/*container.addEventListener('mouseover', function() {
+      overRenderer = true;
+    }, false);
+
+    container.addEventListener('mouseout', function() {
+      overRenderer = false;
+    }, false);
+    */
+
+      
+  }
+//  scene = new THREE.Scene();
+//  
+//  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+//  camera.position.y = 150;
+//  camera.position.z = 500;
+//    
+//  renderer = new THREE.CanvasRenderer();
+//  console.log(renderer);
+//  renderer.setSize(window.innerWidth, window.innerHeight);
+//  
+//  var light = new THREE.PointLight( 0xffffff);
+//  light.position.set(500,500,500);
+//  scene.add(light);
+//  
+//  var geometry = new THREE.CubeGeometry(100,100,100);
+//  var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+//  var cube = new THREE.Mesh(geometry, material);
+//  scene.add(cube);
+  //console.log(cube);
+//  
+//  console.log(renderer.domElement);
+//  container.append(renderer.domElement);
+//  
+
+  //var globe = new DAT.Globe( container );
+  //console.log(globe);
   
-  renderer.render(scene, camera);
+  //renderer.render(scene, camera);
 
 }
-
-var render = function () {
-    requestAnimationFrame(render);
-
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
-
-    renderer.render(scene, camera);
-};
