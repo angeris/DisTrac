9eb4ef18c4c55d61b22657622d338e3798322e1a
clay-to-n <cbrand@usc.edu>
2015-02-22 07:32:47 -0800

    Updating UI wheeee

--- DisTracApp/client/templates/control/control.js
+++ DisTracApp/client/templates/control/control.js
@@ -7,20 +7,27 @@ Template.control.rendered = function () {
 }
   
 Template.control.helpers({
-    totalSusceptible: function() {
-      
-    },
-    
-    totalInfected: function() {
-      var sum = 0;
-      console.log(Points.find());
-      Points.find().forEach(function(point) {
-        console.log(point.count);
-        sum += point.count;
-      });
-      
-      console.log(sum);
-      return 100;
-    },
+  totalSusceptible: function() {
+
+  },
+
+  totalInfected: function() {
+    var sum = 0;
+    console.log(Points.find());
+    Points.find().forEach(function(point) {
+      console.log(point.count);
+      sum += point.count;
+    });
+
+    console.log(sum);
+    return 100;
+  },
+
+  currentDay: function() {
+    return 5;
+  },
   
+  daySliderValue: function() {
+    return 5; 
+  }
 });
