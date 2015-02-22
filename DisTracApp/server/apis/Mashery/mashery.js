79c571d46171d2f637d2248c407d2d5a3358dd62
Chaitanya Asawa <casawa@stanford.edu>
2015-02-22 04:44:35 -0800

    Mashery start?

--- DisTracApp/server/apis/Mashery/mashery.js
+++ DisTracApp/server/apis/Mashery/mashery.js
@@ -1,4 +1,17 @@
 
 Meteor.methods({
 
+    getEmergencyContact: function(city) {
+      
+       var contactInfo;
+       var hthURL = "https://ghsapi.hthworldwide.com/emergencyinfo/numbers/city/";
+       HTTP.call('GET', hthURL, function(error, data)) {
+           if(error)
+             console.log(error);
+
+       }
+
+
+       return contactInfo;
+    }
 });
